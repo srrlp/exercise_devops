ff64a0bd754e06abb04280cf132dd363bddac3b2
maymae <60160007@go.buu.ac.th>
2020-11-08 15:47:54 +0700

    Add unit-test

--- tests/unit-test.js
+++ tests/unit-test.js
@@ -2,18 +2,6 @@ const assert = require('assert')
 const request = require('supertest')
 const app = require('../index')
 
-describe('Validate setting the test', () => {
-    it('should be ok', () => {
-        assert.ok(true)
-    })
-})
-
-describe('GET "/"', () => {
-    it('respond with Hello!', (done) => {
-        request(app).get('/').expect('Hello!', done)
-    })
-})
-
 describe('GET "/item"', () => {
     it('respond with array of item', (done) => {
         let want = [
